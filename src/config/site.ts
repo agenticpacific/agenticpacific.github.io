a7de9ca0544627ad134e6174c74edab2661d8490
Sachindra Singh <sachin1618@gmail.com>
2026-03-30 11:43:49 +1200

    Initial Astro Commit.

---
+++ src/config/site.ts
@@ -0,0 +1,31 @@
+import ogImage from "@/assets/og-image.png";
+
+export const siteConfig = {
+  name: "Agentic Pacific (FJ)",
+  description:
+    "AI-Augmented Solutions and Services to Enable the Growing Knowledge Sector of the Pacific Islands.",
+  url: "https://agenticpacific.github.io",
+  lang: "en",
+  locale: "en_US",
+  author: "Sachindra Singh",
+  twitter: "@sachin1618",
+  ogImage: ogImage,
+  socialLinks: {
+    twitter: "https://twitter.com/sachin1618",
+    github: "https://github.com/sopac",
+    //discord: "https://discord.com",
+  },
+  navLinks: [
+    { text: "Home", href: "/" },
+    { text: "Legal AI", href: "/services" },
+    { text: "FinTech", href: "/services" },
+    { text: "Earth Observations", href: "/services" },
+    { text: "Document Processing", href: "/services" },
+    { text: "Custom AI Agents", href: "/services" },
+    { text: "About", href: "/about" },
+
+    // { text: "Blog", href: "/blog" },
+    // { text: "Contact", href: "/contact" },
+    // { text: "Widgets", href: "/widgets" },
+  ],
+};
