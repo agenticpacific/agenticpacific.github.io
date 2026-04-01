8dec4b119feb5cc91e3ec3bcef3e4713c66e0ec1
Sachindra Singh <sachin1618@gmail.com>
2026-04-01 14:57:26 +1200

    Contact Form Added.

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,16 +3,14 @@ import ogImage from "@/assets/og-image.png";
 export const siteConfig = {
   name: "Agentic Pacific",
   description:
-    "AI-augmented Domain-Specific Solutions for The Pacific Islands | Legal AI, Financial Techh, Document AI, and Earth Observations.",
+    "AI-augmented Domain-Specific Solutions for The Pacific Islands | Legal AI, Financial Tech, Workflow Automations, Document AI, and Earth Observations.",
   url: "https://agenticpacific.github.io",
   lang: "en",
   locale: "en_US",
   author: "Agentic Pacific",
-  twitter: "@sachin1618",
   ogImage: ogImage,
   socialLinks: {
-    twitter: "https://twitter.com/sachin1618",
-    github: "https://github.com/sopac",
+    github: "https://github.com/agenticpacific/",
     email: "mailto:agentic.pacific@icloud.com",
   },
   navLinks: [
